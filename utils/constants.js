8c7cd933f3d3e0d325c3801811d4d609a33f5c77
Litus <carlesfarpierres@MacBook-Pro-de-Carles.local>
2025-09-26 20:58:43 +0200

    upddate experience

--- utils/constants.js
+++ utils/constants.js
@@ -1,40 +1,95 @@
 export const fileTree = [
-    {
-      id: 1,
-      label: 'src',
-      open: true,
-      children: [
-        {
-          id: 2,
-          label: 'components',
-          open: false,
-          children: [
-            { id: 3, label: 'Terminal.vue', type: 'file' },
-            { id: 4, label: 'Particles.vue', type: 'file' },
-            { id: 5, label: 'FileNode.vue', type: 'file' },
-            { id: 6, label: 'CodeEditor.vue', type: 'file' },
-            { id: 7, label: 'SplitContainer.vue', type: 'file' },
-            { id: 8, label: 'TimeLine.vue', type: 'file'},
-            { id: 9, label: 'CardDetail.vue', type: 'file'},
-            { id: 10, label: 'ToolTip.vue', type: 'file' }
-          ]
-        },
-        { id: 11, label: 'app.vue', type: 'file' }, 
-      ]
-    }
-  ];
+  {
+    id: 1,
+    label: "src",
+    open: true,
+    children: [
+      {
+        id: 2,
+        label: "components",
+        open: false,
+        children: [
+          { id: 3, label: "Terminal.vue", type: "file" },
+          { id: 4, label: "Particles.vue", type: "file" },
+          { id: 5, label: "FileNode.vue", type: "file" },
+          { id: 6, label: "CodeEditor.vue", type: "file" },
+          { id: 7, label: "SplitContainer.vue", type: "file" },
+          { id: 8, label: "TimeLine.vue", type: "file" },
+          { id: 9, label: "CardDetail.vue", type: "file" },
+          { id: 10, label: "ToolTip.vue", type: "file" },
+        ],
+      },
+      { id: 11, label: "app.vue", type: "file" },
+    ],
+  },
+];
 
 export const experience = [
-    {title: 'TITULO', description: 'card-detail.azertium', year: '2021-actually', img: '/assets/azertium_it.png', text: 'AZERTIUM IT'},
-    {title: 'TITULO', description: 'card-detail.t2client', year: '2020-21', img: '/assets/t2client.png', text: 'T2CLIENT'},
-    {title: 'TITULO', description: 'card-detail.coneptum', year: '2020-20', img: '/assets/coneptum.png', text: 'CONEPTUM AI'},
-    {title: 'TITULO', description: 'card-detail.casals-online', year: '2017-19', img: '/assets/casals_online.png', text: 'CASALS ONLINE'}
-  ]
-  // {title: 'TITULO', description: 'card-detail.pinturasfar', year: '2004-16', img: '/assets/pinturas_far.png', text: 'PINTURAS FAR'},
+  {
+    title: "TITULO",
+    description: "card-detail.bershka",
+    year: "2025-actually",
+    img: "/assets/bershka_white.png",
+    text: "BERSHKA",
+  },
+  {
+    title: "TITULO",
+    description: "card-detail.azertium",
+    year: "2021-25",
+    img: "/assets/azertium_it.png",
+    text: "AZERTIUM IT",
+  },
+  {
+    title: "TITULO",
+    description: "card-detail.t2client",
+    year: "2020-21",
+    img: "/assets/t2client.png",
+    text: "T2CLIENT",
+  },
+  {
+    title: "TITULO",
+    description: "card-detail.coneptum",
+    year: "2020-20",
+    img: "/assets/coneptum.png",
+    text: "CONEPTUM AI",
+  },
+  {
+    title: "TITULO",
+    description: "card-detail.casals-online",
+    year: "2017-19",
+    img: "/assets/casals_online.png",
+    text: "CASALS ONLINE",
+  },
+];
+// {title: 'TITULO', description: 'card-detail.pinturasfar', year: '2004-16', img: '/assets/pinturas_far.png', text: 'PINTURAS FAR'},
 
 export const academy = [
-  {description: 'card-detail.campus', text: 'CAMPUS MVP', year: '2019-20', img: '/assets/campusmvp.png', link: 'https://www.campusmvp.es/certificados/carles-far-pierres/4701'},
-  {description: 'card-detail.isdi', text: 'ISDI CODERS', year: '2016-17', img: '/assets/isdicoders.png', link: 'https://isdicoders.com/'},
-  {description: 'card-detail.stucom', text: 'STUCOM', year: '2013-17', img:'/assets/stucom.png', link: 'https://www.stucom.com/'},
-  {description: 'card-detail.ub', text: 'UB UNIVERSITAT', year: '2012-13', img: '/assets/ub.png', link: ''}
-]
+  {
+    description: "card-detail.campus",
+    text: "CAMPUS MVP",
+    year: "2019-20",
+    img: "/assets/campusmvp.png",
+    link: "https://www.campusmvp.es/certificados/carles-far-pierres/4701",
+  },
+  {
+    description: "card-detail.isdi",
+    text: "ISDI CODERS",
+    year: "2016-17",
+    img: "/assets/isdicoders.png",
+    link: "https://isdicoders.com/",
+  },
+  {
+    description: "card-detail.stucom",
+    text: "STUCOM",
+    year: "2013-17",
+    img: "/assets/stucom.png",
+    link: "https://www.stucom.com/",
+  },
+  {
+    description: "card-detail.ub",
+    text: "UB UNIVERSITAT",
+    year: "2012-13",
+    img: "/assets/ub.png",
+    link: "",
+  },
+];
--- utils/templates.ts
+++ utils/templates.ts
@@ -1152,3 +1152,49 @@ onMounted(() => {
 
 </script>
 `
+
+// TO-DO: Implement this function
+const getCategoryPath = (categories, categoryName) => {
+  for (category of categories ) {
+    if (category.name === categoryName) {
+      return `/${category.name}`
+    }
+
+    if (category.subcategories.length > 0) {
+        const subcategoryPath = getCategoryPath(category.subcategories, categoryName)
+        if (subcategoryPath) {
+          return `/${category.name}/${subcategoryPath}`
+        }
+    }
+  }
+  return null
+}
+
+const categories = [
+  {
+      name: 'category1',
+      subcategories: [
+          {
+              name: 'category2',
+              subcategories: []
+          },
+          {
+              name: 'category3',
+              subcategories: [
+                  {
+                      name: 'category4',
+                      subcategories: []
+                  }
+              ]
+          }
+      ]
+  },
+  {
+      name: 'category5',
+      subcategories: []
+  }
+];
+// OUTPUT SAMPLES
+console.log(getCategoryPath(categories, 'category4')); // should output: '/category1/category3/category4'
+console.log(getCategoryPath(categories, 'category2')); // should output: '/category1/category2'
+console.log(getCategoryPath(categories, 'category5')); // should output: '/category5'
