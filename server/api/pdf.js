5690bed8e887ad0b71d4670b6f5e6b2a53f9da4c
Litus <carlesfarpierres@MacBook-Pro-de-Carles.local>
2025-12-15 23:36:11 +0100

    updated cv

--- server/api/pdf.js
+++ server/api/pdf.js
@@ -1,13 +1,13 @@
-import { createReadStream } from 'fs';
-import { resolve } from 'path';
+import { createReadStream } from "fs";
+import { resolve } from "path";
 
 export default defineEventHandler((event) => {
-  const filePath = resolve('public/files/cv_carlesfar.pdf');
+  const filePath = resolve("public/files/cv_carles.pdf");
   const stream = createReadStream(filePath);
 
   setHeaders(event, {
-    'Content-Type': 'application/png',
-    'Content-Disposition': 'attachment; filename="cv_carlesfar.pdf"',
+    "Content-Type": "application/png",
+    "Content-Disposition": 'attachment; filename="cv_carles.pdf"',
   });
 
   return stream;
